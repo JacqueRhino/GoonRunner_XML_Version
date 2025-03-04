a3f625045e98376056f7b09bb5f8ca93cd3f5552
El Vinlos <vuongkhanhvinh205@gmail.com>
2025-03-05 02:11:59 +0700

    First Commit

---
+++ GoonRunner/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+using System.Windows;
+
+namespace GoonRunner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
