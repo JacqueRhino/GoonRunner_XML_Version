9d0ff302ee92234a465f04f2c5213fecb16c2028
Khang <fatkhang2005@gmail.com>
2025-04-06 04:53:00 +0700

    Xóa mọe Base64

---
+++ GoonRunner/MVVM/Model/DataProvider.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace GoonRunner.MVVM.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins { get { if (_ins == null) _ins = new DataProvider(); return _ins; } set { _ins = value; } }
+        public GoonRunnerEntities goonRunnerDB { get; set; }
+        private DataProvider()
+        {
+            goonRunnerDB = new GoonRunnerEntities();
+        }
+    }
+}
---
+++ GoonRunner/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoonRunner.MVVM.View;
+using System.Windows.Input;
+using System.Windows;
+
+namespace GoonRunner.MVVM.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool isLoaded { get; set; }
+        public ICommand LoadedWindowCommand { get; set; }
+
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                isLoaded = true;
+                if (p == null)
+                    return;
+                LogIn loginWindow = new LogIn();
+                loginWindow.ShowDialog();
+
+                if (loginWindow.DataContext == null)
+                    return;
+                var loginWM = loginWindow.DataContext as LoginViewModel;
+                if (loginWM.isLogin)
+                    p.Show();
+                else
+                    p.Close();
+            });
+        }
+    }
+}
